f88fc48e5bb8264f4bb8f0a06cbb2c618d41c173
DivineDomokuma10 <divinedomokuma@gmail.com>
2023-12-13 18:29:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SubRouteContainer from "../../components/subRouteContainer";
+
+const Profile = () => {
+  return <SubRouteContainer>Profile</SubRouteContainer>;
+};
+
+export default Profile;
